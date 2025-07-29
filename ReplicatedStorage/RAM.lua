917f7fb29610057639dcae390e82ad5843aa227e
juaninsanote7773 <estudiopapus1234@gmail.com>
2025-07-29 13:46:21 -0300

    Update RAM.lua

--- ReplicatedStorage/RAM.lua
+++ ReplicatedStorage/RAM.lua
@@ -2,13 +2,23 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local RAM = {}
 
-local size_bytes = 32000 --1kb
+RAM.RAMSTOR = {}
+
+local size_bytes = 1024 --1kb
 
 --local inst_set = {"load", "add", "set", "cmp", "JA", "JL", "JE", "jmp", "out", "in"}
 
+function RAM.write(i, value)
+	RAM.RAMSTOR[i] = value
+end
+
+function RAM.read(i)
+	return RAM.RAMSTOR[i]
+end
+
 function RAM.generate()
 	for i = 1, size_bytes do
-		local clone = ReplicatedStorage["RAM-Adress"]:Clone()
+		--[[local clone = ReplicatedStorage["RAM-Adress"]:Clone()
 		clone.Parent = ReplicatedStorage["RAM-Storage"]
 		clone.Name = string.format("0x%02X", i)
 		--clone.Name = i
@@ -17,7 +27,9 @@ function RAM.generate()
 		
 		--if i < #inst_set then
 		--	clone.data.Value = inst_set[i]
-		--end
+		--end]]
+		
+		table.insert(RAM.RAMSTOR, "")
 	end
 end
 
