93cd45640c897ef0cf715e1ac40e966453079697
juaninsanote7773 <estudiopapus1234@gmail.com>
2025-07-30 19:34:16 -0300

    Delete src directory

--- src/client/LocalScript.client.lua
+++
@@ -1,6 +0,0 @@
-local UserInputService = game:GetService("UserInputService")
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
-UserInputService.InputBegan:Connect(function(input)
-	ReplicatedStorage.Input:FireServer(input.KeyCode)
-end)
