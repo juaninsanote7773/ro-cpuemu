2f0a8b831df974d4f585cb0ee4fc27374c32b350
juaninsanote7773 <estudiopapus1234@gmail.com>
2025-07-29 23:20:56 -0300

    Update PROCESS.lua

--- ReplicatedStorage/PROCESS.lua
+++ ReplicatedStorage/PROCESS.lua
@@ -9,6 +9,8 @@ local PROCESS = {}
 function PROCESS.start_comp(compiled)
 	local origin = 0
 	local def_origin = false
+	
+	local data_place = 0
 	for i,v in pairs(compiled) do
 		local data = v[1]
 		local data_type = v[2]
@@ -21,24 +23,34 @@ function PROCESS.start_comp(compiled)
 		if def_origin == true then
 			def_origin = false
 			origin = tonumber(data)
+			if not origin then
+				origin = 0
+				continue
+			end
 			print(origin.." THIS IS THE ORIGIN")
 			continue
 		end
 		
+		if origin == 0 then
+			data_place = ReplicatedStorage["RAM-LastSpace"].Value
+		else
+			data_place = origin+i
+		end
+		
 		if data_type == 0 then
-			CPUINST.set(ReplicatedStorage["RAM-LastSpace"].Value+origin, data, data_type)
+			CPUINST.set(data_place, data, data_type)
 		end
 		if data_type == 1 then
-			CPUINST.set(ReplicatedStorage["RAM-LastSpace"].Value+origin, data, data_type)
+			CPUINST.set(data_place, data, data_type)
 		end
 		if data_type == 2 then
-			CPUINST.set(ReplicatedStorage["RAM-LastSpace"].Value+origin, data, data_type)
+			CPUINST.set(data_place, data, data_type)
 		end
 		if data_type == 3 then
-			CPUINST.set(ReplicatedStorage["RAM-LastSpace"].Value+origin, data, data_type)
+			CPUINST.set(data_place, data, data_type)
 		end
 		if data_type == 4 then
-			CPUINST.set(ReplicatedStorage["RAM-LastSpace"].Value+origin, data, data_type)
+			CPUINST.set(data_place, data, data_type)
 		end
 		
 	end
